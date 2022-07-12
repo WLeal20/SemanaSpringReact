6aefb698990dc4440b45766526db8a45f5d3c4c7
Wanderlan Leal <wanderlan.leal@gmail.com>
2022-07-11 23:03:01 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import NotificationButton from "./componentes/NotificationButton"
 
 function App() {
   return(
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
 
   )
 }
